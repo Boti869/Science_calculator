2601046406111323138314ee2e0528276fce72c7
Boti869 <botondlevai@gmail.com>
2025-08-09 18:39:52 +0200

    Looks like I'm going backward, but trust the process

---
+++ calcs/Science-calc.c
@@ -0,0 +1,10 @@
+#include"../header_files/calc.h"
+#include"../header_files/initLaws.h"
+#include<math.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+double sciCalc() {
+    
+}
---
+++ calcs/basic-calc.c
@@ -0,0 +1,43 @@
+//this is one of my first projects modified to fit here    
+#include<stdio.h>
+#include<math.h>
+#include"../header_files/calc.h"
+
+double calc() {
+    double num1;
+    double num2;
+    char op;
+
+    printf("Enter the first number: ");
+    scanf("%lf", &num1);
+
+    printf("Enter the operator: ");
+    scanf(" %c", &op);
+
+    printf("Enter the second number: ");
+    scanf("%lf", &num2);
+
+    if(op == '+') {
+        printf("%f", num1 + num2);
+        return num1 + num2;
+    }
+    else if(op == '-') {
+        printf("%f", num1 - num2);
+        return num1 - num2;
+    }
+    else if(op == '*') {
+        printf("%.5f", num1 * num2);
+        return num1 * num2;
+    }
+    else if(op == '/') {
+        if(num2 == 0){printf("Cannot divide by zero");}
+        else{   
+        printf("%.5f", num1 / num2);
+        return num1 / num2;}
+    }
+    else if(op == '^') {
+        printf("%.5f", pow(num1, num2));
+        return pow(num1, num2);
+    }
+    else{printf("invalid operator");}
+}
